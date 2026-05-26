7ee56d0e8f85a16e4ff0ae5471d49421b75ae5fd
Nandakumarsett <137186246+Nandakumarsett@users.noreply.github.com>
2026-05-26 11:45:47 +0530

    fix: global safe Date polyfill to resolve ios safari crashes at the root level

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,40 @@
+// Safe Date Polyfill for iOS Safari (WebKit) SQL Date parsing bug
+const NativeDate = globalThis.Date;
+
+function PatchedDate(...args) {
+  // If called without 'new', return the string representation (standard JS behavior)
+  if (!new.target) {
+    return NativeDate();
+  }
+  
+  // If called with a SQL string, fix it
+  if (args.length === 1 && typeof args[0] === 'string') {
+    if (/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/.test(args[0])) {
+      return new NativeDate(args[0].replace(' ', 'T'));
+    }
+  }
+  
+  // Construct normally
+  return new NativeDate(...args);
+}
+
+// Copy static methods
+PatchedDate.now = NativeDate.now;
+PatchedDate.UTC = NativeDate.UTC;
+
+PatchedDate.parse = function(s) {
+  if (typeof s === 'string' && /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/.test(s)) {
+    return NativeDate.parse(s.replace(' ', 'T'));
+  }
+  return NativeDate.parse(s);
+};
+
+// Prototype chain maintenance
+PatchedDate.prototype = NativeDate.prototype;
+PatchedDate.prototype.constructor = PatchedDate;
+
+globalThis.Date = PatchedDate;
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -21,3 +58,4 @@ if ('serviceWorker' in navigator) {
     })
   })
 }
+
