4b4a32a2e88b2b9a5f4a1e4c0bb06a08d554b087
Nandakumarsett <137186246+Nandakumarsett@users.noreply.github.com>
2026-07-28 22:54:48 +0530

    style: increase KW logo mark size to match brand name text height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,7 +126,7 @@ export default function Navbar({ onOrderClick, onAuthClick, isDark = true }) {
           <img 
             src="/logo-square.png" 
             alt="KnoWMi Logo" 
-            className="w-11 h-11 sm:w-[54px] sm:h-[54px] lg:w-[58px] lg:h-[58px] object-contain shrink-0 group-hover:scale-105 transition-transform"
+            className="w-[52px] h-[52px] sm:w-[62px] sm:h-[62px] lg:w-[68px] lg:h-[68px] object-contain shrink-0 group-hover:scale-105 transition-transform"
             style={useDarkTheme ? { filter: 'invert(1) hue-rotate(180deg)' } : {}}
           />
 
